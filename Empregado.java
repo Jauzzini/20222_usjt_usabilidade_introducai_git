b667bdf379de4a28495fffe93538af200a141cdb
Jairo Mazini <jairo.mazini@hotmail.com>
2022-08-11 20:38:21 -0300

    Comiit inicial. Classe Empregado Criada.

---
+++ Empregado.java
@@ -0,0 +1,16 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    } 
+
+    public static void setNome() {
+        this.        
+    }
+}
